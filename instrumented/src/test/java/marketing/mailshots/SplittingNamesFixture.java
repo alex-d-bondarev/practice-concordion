734bb81008eb10630041c186379fdfce98342cd8
Alex <alex.d.bondarev@gmail.com>
2019-12-22 16:21:14 +0200

    Coding step

--- instrumented/src/test/java/marketing/mailshots/SplittingNamesFixture.java
+++ instrumented/src/test/java/marketing/mailshots/SplittingNamesFixture.java
@@ -5,4 +5,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
 public class SplittingNamesFixture {
+
+    public Result split(String fullName) {
+        Result result = new Result();
+        String[] words = fullName.split(" ");
+        result.firstName = words[0];
+        result.lastName = words[1];
+        return result;
+    }
+
+    class Result {
+        public String firstName;
+        public String lastName;
+    }
 }
